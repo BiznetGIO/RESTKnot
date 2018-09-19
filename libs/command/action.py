2130e3077a22b8cf609b499a0980d4d14e0891e7
Sofyan Saputra <meongbego@gmail.com>
2018-09-20 02:08:09 +0700

    Add: New Structure Libs, Template, SDL

---
+++ libs/command/action.py
@@ -0,0 +1,50 @@
+import json
+from libknot.control import *
+
+
+load_lib("libknot.so.7")
+ctl = KnotCtl()
+ctl.connect("/var/run/knot/knot.sock")
+
+
+def block_command(cmd=None, flags=None,section=None,
+                    item=None, identifier=None, zone=None,
+                    owner=None, ttl=None, rtype=None, data=None,
+                    filter=None):
+    try:
+        ctl.send_block(cmd="conf-begin")
+        resp = ctl.receive_block()
+        ctl.send_block(cmd="conf-set", section="zone", item="domain", data="test123")
+        resp = ctl.receive_block()
+        ctl.send_block(cmd="conf-commit")
+        resp = ctl.receive_block()
+        ctl.send_block(cmd="conf-read", section="zone", item="domain")
+        resp = ctl.receive_block()
+        print(json.dumps(resp, indent=4))
+    finally:
+        ctl.send(KnotCtlType.END)
+        ctl.close()
+
+
+def stats_command(cmd, flags=None, section=None,
+                    item=None, identifier=None, zone=None,
+                    owner=None, ttl=None, rtype=None, data=None,
+                    filter=None):
+    stats_data = dict()
+    try:
+        ctl.send_block(cmd=cmd, flags=flags, section=section,
+                    item=item, identifier=identifier, zone=zone,
+                    owner=owner, ttl=ttl, rtype=rtype, data=data,
+                    filter=filter)
+        stats_data = ctl.receive_stats()
+    except:
+        pass
+
+    ctl.send(KnotCtlType.END)
+    ctl.close()
+
+    stats = {
+        "data": stats_data
+    }
+
+    return json.dumps(stats, indent=4, sort_keys=True)
---
+++ libs/utils.py
@@ -0,0 +1,30 @@
+import yaml, os
+
+def yaml_parser(file):
+    with open(file, 'r') as stream:
+        try:
+            data = yaml.load(stream)
+            return data
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def mkdir(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+def read_file(file):
+    with open(file, 'r') as outfile:
+        return outfile.read()
+
+def list_dir(dirname):
+    listdir = list()
+    for root, dirs, files in os.walk(dirname):
+        for file in files:
+            listdir.append(os.path.join(root, file))
+    return listdir
+
+def repodata():
+    abs_path = os.path.dirname(os.path.realpath(__file__))
+    repo_file = "{}/templates/sdml.yml".format(abs_path)
+    return yaml_parser(repo_file)
