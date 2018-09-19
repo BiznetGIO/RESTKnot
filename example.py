e27a3a3a6f166ba087467104d6098cabe4ebb061
Sofyan Saputra <meongbego@gmail.com>
2018-09-20 02:26:57 +0700

    Edit: Example

--- example.py
+++ example.py
@@ -4,5 +4,36 @@ from libs.utility import utils
 yaml = utils.yaml_parser('knot.yml')
 data_yaml = parser.initialiaze(data=yaml)
 print(data_yaml)
-# data_json = initialiaze(data=json_req)
+
+# HTTP REQUEST
+# json_req={
+#   "configbegin": {
+#     "sendblock": {
+#       "cmd": "conf-begin"
+#     },
+#     "receive": {
+#       "type": "block"
+#     }
+#   },
+#   "configset": {
+#     "sendblock": {
+#       "cmd": "conf-set",
+#       "section": "zone",
+#       "item": "domain",
+#       "data": "tes123"
+#     },
+#     "receive": {
+#       "type": "block"
+#     }
+#   },
+#   "configcommit": {
+#     "sendblock": {
+#       "cmd": "conf-commit"
+#     },
+#     "receive": {
+#       "type": "block"
+#     }
+#   }
+# }
+# data_json = parser.initialiaze(data=json_req)
 # print(data_json)
