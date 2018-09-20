ad56ddd6e5ed93358df2963a58d41b50bdcf0f2d
Sofyan Saputra <meongbego@gmail.com>
2018-09-20 19:41:21 +0700

    Edit: Change To Access Control Libknot

--- libs/command/action.py
+++ libs/command/action.py
@@ -1,54 +1,75 @@
-import json
-from libs.command.libknot.control import *
+import json, os
+from libs.command.libknot import control
 
 
-load_lib("libknot.so.7")
-ctl = KnotCtl()
-ctl.connect("/var/run/knot/knot.sock")
-
-
-def block_command(cmd=None, flags=None,section=None,
-                    item=None, identifier=None, zone=None,
-                    owner=None, ttl=None, rtype=None, data=None,
-                    filter=None):
-    print(cmd)
-    print(section)
-    print(item)
-    print(data)
-    # try:
-    #     ctl.send_block(cmd="conf-begin")
-    #     resp = ctl.receive_block()
-    #     # ctl.send_block(cmd="conf-set", section="zone", item="domain", data="test12")
-    #     # resp = ctl.receive_block()
-    #     # ctl.send_block(cmd="conf-commit")
-    #     # resp = ctl.receive_block()
-    #     # ctl.send_block(cmd="conf-read", section="zone", item="domain")
-    #     # resp = ctl.receive_block()
-    #     print(json.dumps(resp, indent=4))
-    # finally:
-    #     ctl.send(KnotCtlType.END)
-    #     ctl.close()
-
-
-def stats_command(cmd, flags=None, section=None,
-                    item=None, identifier=None, zone=None,
-                    owner=None, ttl=None, rtype=None, data=None,
-                    filter=None):
-    stats_data = dict()
+def check_libs(libs):
     try:
-        ctl.send_block(cmd=cmd, flags=flags, section=section,
-                    item=item, identifier=identifier, zone=zone,
-                    owner=owner, ttl=ttl, rtype=rtype, data=data,
-                    filter=filter)
-        stats_data = ctl.receive_stats()
-    except:
-        pass
+        libs_value = os.system("ls /lib64/ | grep "+libs+"")
+    except Exception as e:
+        print(e)
+    else:
+        return libs_value
 
-    ctl.send(KnotCtlType.END)
-    ctl.close()
+def load_libknot(libknot=None):
+    libs = None
+    try:
+        libs = libknot
+    except Exception as e:
+        print(e)
+        check_libs('libknot')
+    else:
+        control.load_lib(libs)
 
-    stats = {
-        "data": stats_data
-    }
+def load_control():
+    ctl = control.KnotCtl()
+    return ctl
 
-    return json.dumps(stats, indent=4, sort_keys=True)
+def load_sock(knot_sock):
+    ctl = load_control()
+    try:
+        ctl.connect(knot_sock)
+    except Exception as e:
+        print(e)
+
+def send_block(params):
+    ctl = load_control()
+    try:
+        ctl.send_block(**params)
+    except Exception as e:
+        print(e)
+
+def receive(params):
+    ctl = load_control()
+    try:
+        resp = ctl.receive(**params)
+    except Exception as e:
+        print(e)
+    else:
+        return resp
+
+def receive_block():
+    ctl = load_control()
+    try:
+        resp = ctl.receive_block()
+    except Exception as e:
+        print(e)
+    else:
+        return resp
+
+def receive_stats():
+    ctl = load_control()
+    try:
+        resp = ctl.receive_stats()
+    except Exception as e:
+        print(e)
+    else:
+        return resp
+
+def send():
+    ctl = load_control()
+    try:
+        ctl.send(control.KnotCtlType.END)
+    except Exception as e:
+        print(e)
+    finally:
+        ctl.close()
