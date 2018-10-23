47280b76988e5c05f3a37f7727584fdef780328e
Sofyan Saputra <meongbego@gmail.com>
2018-10-23 10:49:27 +0700

    Add: Where Data Function Domain

--- api/app/models/domain.py
+++ api/app/models/domain.py
@@ -7,6 +7,7 @@ dbname = os.getenv("INFLUXDB_DATABASE")
 measure_name = "domain"
 tag_key = hashlib.md5(str(db.timeset()).encode('utf-8')).hexdigest()
 
+
 def insert(data=None):
     data_prepare = [{
         "measurement": measure_name,
@@ -31,3 +32,58 @@ def insert(data=None):
             "data": data
         }
         return respon
+
+def delete(tag_id):
+    data_prepare={
+        "domain_id": tag_id
+    }
+    try:
+        status = db.delete(dbname=dbname,
+            measurement=measure_name,
+            tags=data_prepare)
+    except Exception as e:
+        respon = {
+            "status": False,
+            "messages": str(e)
+        }
+        return respon
+    else:
+        respon = {
+            "status": status,
+            "measurement": measure_name,
+            "data": data_prepare
+        }
+        return respon
+
+def all():
+    try:
+        dm = db.query(dbname,"SELECT * FROM "+measure_name)
+        data_points = list(dm.get_points(measurement=measure_name))
+    except Exception as e:
+        respon = {
+            "messages": str(e)
+        }
+        return respon
+    else:
+        respon = {
+            "measurement": measure_name,
+            "data": data_points
+        }
+        return respon
+
+def where_data(tags):
+    try:
+        dm = db.query(dbname,"SELECT * FROM "+measure_name)
+        data_points = list(dm.get_points(measurement=measure_name, tags=tags))
+    except Exception as e:
+        respon = {
+            "messages": str(e)
+        }
+        return respon
+    else:
+        respon = {
+            "measurement": measure_name,
+            "data": data_points
+        }
+        return respon
+
