aafc4d170ace2d76484656a5a2522829472ca15a
Sofyan Saputra <meongbego@gmail.com>
2018-10-25 10:35:55 +0700

    Add: Endpoint TTL

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 from .domain import *
 from .record_name import *
+from .ttl import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
 
 api.add_resource(DomainCommand, '/domain')
 api.add_resource(RecordName, '/recordname')
+api.add_resource(TtlName, '/ttl')
