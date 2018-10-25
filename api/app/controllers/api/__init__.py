41f71834c5b100d8d1f25cab38951bf463611f20
Sofyan Saputra <meongbego@gmail.com>
2018-10-25 17:36:56 +0700

    Add: Endpoint Zone And Record Data

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -3,10 +3,14 @@ from flask_restful import Api
 from .domain import *
 from .record_name import *
 from .ttl import *
+from .zone import *
+from .record_data import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
 
 api.add_resource(DomainCommand, '/domain')
-api.add_resource(RecordName, '/recordname')
+api.add_resource(RecordName, '/namerecord')
 api.add_resource(TtlName, '/ttl')
+api.add_resource(ZoneName, '/zone')
+api.add_resource(RecordData, '/datarecord')
