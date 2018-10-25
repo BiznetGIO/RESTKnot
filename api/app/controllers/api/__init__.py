1e4b09560b00af44bfd802dc8c9972b858a1e633
Sofyan Saputra <meongbego@gmail.com>
2018-10-25 09:30:26 +0700

    Add: Adding, Remove, Get All, Get By ID Domain

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -6,5 +6,4 @@ from .domain import *
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
 
-api.add_resource(DomainAll, '/domain')
 api.add_resource(DomainCommand, '/domain')
--- api/app/controllers/api/domain.py
+++ api/app/controllers/api/domain.py
@@ -1,29 +1,41 @@
 from flask_restful import Resource, reqparse, request
 from app.helpers.rest import *
-from app.models import domain_models as dm
+from app.models import api_models as db
 from app.helpers import cmd_parser as cmd
+from app.libs import utils
 
 
-class DomainAll(Resource):
+class DomainCommand(Resource):
     def get(self):
-        # status = domain.insert(data={
-        #     "domain_name": "rosa.com.",
-        # })
-        # print(status)
-
         # status = domain.delete("domain_d0cab4cc907e6b6fe714c7f477644d9b")
-
+        command = utils.get_command(request.path)
         try:
-            respons = dm.result()
-        except Exception as e:
+            respons = db.result(command)
+        except Exception:
             respons = None
         else:
             return response(200, data=respons)
 
-class DomainCommand(Resource):
+
     def post(self):
         json_req = request.get_json(force=True)
-        cmd.parser(json_req)
-
-        respons = ""
+        command = utils.get_command(request.path)
+        init_data = cmd.parser(json_req, command)
+        respons = dict()
+        if init_data['action'] == 'insert':
+            respons = db.insert(init_data['data'])
+        elif init_data['action'] == 'where':
+            measurement = ""
+            tags = dict()
+            for i in init_data['data']:
+                measurement = i['measurement']
+                tags = i['tags']
+            respons = db.row(measurement,tags)
+        elif init_data['action'] == 'remove':
+            measurement = ""
+            tags = dict()
+            for i in init_data['data']:
+                measurement = i['measurement']
+                tags = i['tags']
+            respons = db.delete(measurement,tags)
         return response(200, data=respons)
