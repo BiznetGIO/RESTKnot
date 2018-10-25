9cd4ca3f9c7874fbb5877dbb6230ca8a6fefba67
Sofyan Saputra <meongbego@gmail.com>
2018-10-25 18:01:02 +0700

    Add: TTL Data Endpoint

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -5,6 +5,7 @@ from .record_name import *
 from .ttl import *
 from .zone import *
 from .record_data import *
+from .ttl_data import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
@@ -14,3 +15,4 @@ api.add_resource(RecordName, '/namerecord')
 api.add_resource(TtlName, '/ttl')
 api.add_resource(ZoneName, '/zone')
 api.add_resource(RecordData, '/datarecord')
+api.add_resource(TtlData, '/datattl')
