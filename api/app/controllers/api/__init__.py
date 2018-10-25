c80d952f6f48b609211ae4baca63fdbe252dbcc1
Sofyan Saputra <meongbego@gmail.com>
2018-10-25 10:24:59 +0700

    Add: Endpoint Recordname

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_restful import Api
 from .domain import *
-
+from .record_name import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
 
 api.add_resource(DomainCommand, '/domain')
+api.add_resource(RecordName, '/recordname')
