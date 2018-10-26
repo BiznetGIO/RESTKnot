8c6b8c243c6fb273ff6fb6480163f67e717a2c58
Sofyan Saputra <meongbego@gmail.com>
2018-10-26 10:31:21 +0700

    Add: Endpoint content And Content Data Serial

--- api/app/controllers/api/__init__.py
+++ api/app/controllers/api/__init__.py
@@ -6,6 +6,8 @@ from .ttl import *
 from .zone import *
 from .record_data import *
 from .ttl_data import *
+from .content import *
+from .content_data import *
 
 api_blueprint = Blueprint("api", __name__, url_prefix='/api')
 api = Api(api_blueprint)
@@ -16,3 +18,5 @@ api.add_resource(TtlName, '/ttl')
 api.add_resource(ZoneName, '/zone')
 api.add_resource(RecordData, '/datarecord')
 api.add_resource(TtlData, '/datattl')
+api.add_resource(ContentName, '/content')
+api.add_resource(ContentData, '/datacontent')
