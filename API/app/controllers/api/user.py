be74d47497bafbac31cb4b4b2b3999328442246e
riszkymf <riszkymfahreza@gmail.com>
2019-01-21 14:46:22 +0700

    Add endpoint for data listing and authentication

--- API/app/controllers/api/user.py
+++ API/app/controllers/api/user.py
@@ -144,7 +144,6 @@ class UserDataZoneInsert(Resource):
             "userdata_id"   : userdata_id
         }
 
-        print(data_insert)
        
         try :
             result = db.insert(table='zn_user_zone', data=data_insert)
