27bbf31d5c90d0724e5f9a0c02f089d34a7f941b
meongbego <iank@localhost.localdomain>
2019-07-02 22:04:46 +0700

    Fix: Notify And ACL Cluster

--- api/app/helpers/cluster_master.py
+++ api/app/helpers/cluster_master.py
@@ -57,7 +57,7 @@ def master_create_json_master(id_zone, nm_config):
     }
     return json_data
 
-def master_create_json_notify(id_zone, nm_config):
+def master_create_json_notify(id_zone, nm_config, urls):
     master_to_slave = None
     if nm_config == "jkt":
         master_to_slave = "cmg"
@@ -68,40 +68,33 @@ def master_create_json_notify(id_zone, nm_config):
     master_to_slave = model.get_by_id("cs_master", "nm_config", master_to_slave)[0]['nm_master']
     data_zone = model.get_by_id("zn_zone", "id_zone", id_zone)[0]
     data_slave = model.get_by_id("v_cs_slave_node", "nm_config", nm_config)
-    data_slave_set = ""
-    # data_slave_set_jkt = ""
+    data_slave_set = []
     for i in data_slave:
         rows = model.get_by_id("v_cs_slave_node", "not nm_config", nm_config)
         for a in rows:
-            if data_slave_set == "":
-                data_slave_set = a['nm_master']
-            else:    
-                data_slave_set = data_slave_set+" "+a['nm_master']
-        if data_slave_set == "":
-            data_slave_set = i['nm_slave_node']
-        else:    
-            data_slave_set = data_slave_set+" "+i['nm_slave_node']
-    json_data = {
-        "master-set-notify": {
+            data_slave_set.append(a['nm_master']) 
+        data_slave_set.append(i['nm_slave_node'])
+    result = []
+    for a in data_slave_set:
+        json_data = {
+            "master-set-notify": {
                 "sendblock": {
-                "cmd": "conf-set",
-                "zone": data_zone['nm_zone'],
-                "item": "notify", 
-                "owner": "",
-                "rtype": "",
-                "ttl": "",
-                "flags": "",
-                "section":"zone",
-                "data": data_slave_set
-            },
+                    "cmd": "conf-set",
+                    "zone": data_zone['nm_zone'],
+                    "item": "notify",
+                    "section":"zone",
+                    "data": a
+                },
                 "receive": {
-                "type": "block"
+                    "type": "block"
+                }
             }
         }
-    }
-    return json_data
+        http_response = utils.send_http(urls, json_data)
+        result.append(http_response)
+    return result
 
-def master_create_json_acl(id_zone, nm_config):
+def master_create_json_acl(id_zone, nm_config, urls):
     master_to_slave = None
     if nm_config == "jkt":
         master_to_slave = "cmg"
@@ -112,38 +105,31 @@ def master_create_json_acl(id_zone, nm_config):
     master_to_slave = model.get_by_id("cs_master", "nm_config", master_to_slave)[0]['nm_master']
     data_zone = model.get_by_id("zn_zone", "id_zone", id_zone)[0]
     data_slave = model.get_by_id("v_cs_slave_node", "nm_config", nm_config)
-    data_slave_set = ""
-    # data_slave_set_jkt = ""
+    data_slave_set = []
     for i in data_slave:
         rows = model.get_by_id("v_cs_slave_node", "not nm_config", nm_config)
         for a in rows:
-            if data_slave_set == "":
-                data_slave_set = a['nm_master']
-            else:    
-                data_slave_set = data_slave_set+" "+a['nm_master']
-        if data_slave_set == "":
-            data_slave_set = i['nm_slave_node']
-        else:    
-            data_slave_set = data_slave_set+" "+i['nm_slave_node']
-    json_data = {
-        "master-set-notify": {
+            data_slave_set.append(a['nm_master']) 
+        data_slave_set.append(i['nm_slave_node'])
+    result = []
+    for a in data_slave_set:
+        json_data = {
+            "master-set-acl": {
                 "sendblock": {
-                "cmd": "conf-set",
-                "zone": data_zone['nm_zone'],
-                "item": "acl", 
-                "owner": "",
-                "rtype": "",
-                "ttl": "",
-                "flags": "",
-                "section":"zone",
-                "data": data_slave_set
-            },
+                    "cmd": "conf-set",
+                    "zone": data_zone['nm_zone'],
+                    "item": "acl",
+                    "section":"zone",
+                    "data": a
+                },
                 "receive": {
-                "type": "block"
+                    "type": "block"
+                }
             }
         }
-    }
-    return json_data
+        http_response = utils.send_http(urls, json_data)
+        result.append(http_response)
+    return result
 
 def set_file_all(id_zone):
     data_zone = model.get_by_id("zn_zone", "id_zone", id_zone)[0]
