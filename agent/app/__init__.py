0df3c0d2d7fd087be2a9c80c0c241683f0605c2f
Sofyan Saputra <meongbego@gmail.com>
2019-07-14 06:31:34 +0700

    Edit: new Method check sync after death agent

--- agent/app/__init__.py
+++ agent/app/__init__.py
@@ -2,15 +2,19 @@ from flask import Flask
 from . import configs
 from flask_cors import CORS
 import os
+from app.middlewares import check_on
 from app.controller import api_blueprint
+
 
 app = Flask(__name__)
 app.config.from_object(configs.Config)
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 CORS(app, resources={r"/api/*": {"origins": "*"}})
-
 app.register_blueprint(api_blueprint)
+
+check_on.check_on_server()
+
 
 
 
