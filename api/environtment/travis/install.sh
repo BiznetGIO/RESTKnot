e27616672d0f977398bbdb6db5c7185bb173c050
riszkymf <riszkymfahreza@gmail.com>
2019-07-23 22:45:22 +0700

    fix travis

--- api/environtment/travis/install.sh
+++ api/environtment/travis/install.sh
@@ -4,3 +4,11 @@ virtualenv -p python3 env
 source env/bin/activate
 pip install -r requirements.txt
 pip install coverage pytest pytest-cov pytest-ordering testfixtures
+redis-cli config set requirepass 'pass'
+mv environtment/travis/env.example .env
+mv environtment/travis/.coveragerc.example .coveragerc
+mv environtment/travis/run_travis.sh run_travis.sh
+mv environtment/travis/deploy.sh deploy.sh
+
+chmod +x run_travis.sh
+./run_travis.sh
