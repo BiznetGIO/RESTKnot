ca3ac616c64c2f51c71492a83847d070de283c20
riszkymf <riszkymfahreza@gmail.com>
2019-07-23 22:23:19 +0700

    merge shell

--- api/environtment/travis/setup.sh
+++ api/environtment/travis/setup.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 
-redis-cli config set requirepass 'pass'
-mv environtment/travis/env.example .env
-mv environtment/travis/.coveragerc.example .coveragerc
-mv environtment/travis/run_travis.sh run_travis.sh
-mv environtment/travis/deploy.sh deploy.sh
