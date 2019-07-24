3dd6f24b5a01f3cc96b36068563916d228a774f0
riszkymf <riszkymfahreza@gmail.com>
2019-07-24 12:02:56 +0700

    fix shell lupa di add tadi

--- api/environtment/travis/deploy.sh
+++ api/environtment/travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login -u "$GITLAB_USER_MY" -p "$GITLAB_PASS_MY" registry.gitlab.com
-docker build -t registry.gitlab.com/riszkymf/restknot:$TRAVIS_TAG .
-docker push registry.gitlab.com/riszkymf/restknot:$TRAVIS_TAG
+docker build -t $TRAVIS_TAG registry.gitlab.com/riszkymf/restknot .
+docker push registry.gitlab.com/riszkymf/restknot
