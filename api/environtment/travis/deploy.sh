a3aa1a6b0eb15209694cecb72f652f75624d3f81
riszkymf <riszkymfahreza@gmail.com>
2019-07-24 13:34:52 +0700

    fix shell 2

--- api/environtment/travis/deploy.sh
+++ api/environtment/travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd api
 docker login -u "$GITLAB_USER_MY" -p "$GITLAB_PASS_MY" registry.gitlab.com
-docker build registry.gitlab.com/riszkymf/restknot:$TRAVIS_TAG .
+docker build -t registry.gitlab.com/riszkymf/restknot:$TRAVIS_TAG .
 docker push registry.gitlab.com/riszkymf/restknot
