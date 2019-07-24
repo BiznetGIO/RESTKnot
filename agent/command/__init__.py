b3e0717e4950d71c20bf1f912f5b7bee4bee7a4e
Sofyan Saputra <meongbego@gmail.com>
2019-07-25 05:04:04 +0700

    Add: Support list execute command

--- agent/command/__init__.py
+++ agent/command/__init__.py
@@ -2,23 +2,8 @@ from .parse import parser
 from .utility import utils
 
 def read_rest(data):
-    initialiaze_command = parser.initialiaze(data)
-    command_result = None
     try:
-        command_result = initialiaze_command[0]['type']
-    except Exception:
-        command_result = None
-
-    if command_result == "cluster" or command_result == "general":
-        response={
-            "result": True,
-            "data": initialiaze_command,
-            "status": "Command Execute",
-            "description": data
-        }
-        return response
-    try:
-        data = parser.execute_command(initialiaze_command)
+        data = parser.initialiaze(data)
     except Exception as e:
         print("ERROR READ REST: ", e)
         response={
@@ -30,7 +15,7 @@ def read_rest(data):
     else:
         response={
             "result": True,
-            "description": initialiaze_command,
+            # "description": initialiaze_command,
             "status": "Command Execute",
             "data": data
         }
