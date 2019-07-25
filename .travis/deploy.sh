ece146cd5d4fceb72e61763d42e1d6d41c4f80fd
riszkymf <riszkymfahreza@gmail.com>
2019-07-25 15:25:16 +0700

    change env to biznet gitlab registry

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd api
-docker login -u "$SECRET_MY_GITLAB_USER=" -p "$SECRET_MY_GITLAB_PASS=" registry.gitlab.com
-docker build -t "$URL_REGISTRY":"$TRAVIS_TAG" .
-docker push "$URL_REGISTRY":"$TRAVIS_TAG"
+docker login -u "$GITLAB_USER" -p "$GITLAB_PASS" registry.gitlab.com
+docker build -t "$URL_REGISTRY_API":"$TRAVIS_TAG" .
+docker push "$URL_REGISTRY_API":"$TRAVIS_TAG"
 cd ../agent
-docker login -u "$SECRET_MY_GITLAB_USER" -p "$SECRET_MY_GITLAB_PASS" registry.gitlab.com
-docker build -t "$URL_REGISTRY":"$TRAVIS_TAG" .
-docker push "$URL_REGISTRY":"$TRAVIS_TAG"
+docker login -u "$GITLAB_USER" -p "$GITLAB_PASS" registry.gitlab.com
+docker build -t "$URL_REGISTRY_AGENT":"$TRAVIS_TAG" .
+docker push "$URL_REGISTRY_AGENT":"$TRAVIS_TAG"
