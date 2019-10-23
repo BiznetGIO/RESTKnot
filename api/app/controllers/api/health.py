0acdcc86bef8212666f9326744a41a037513acf3
azzamsa <azzam@azzamsa.com>
2019-10-23 12:35:01 +0700

    Remove: unused file

--- api/app/controllers/api/health.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Resource
-from app.helpers.rest import response
-
-
-class HealthCheck(Resource):
-    def get(self):
-        data = {"check": "100"}
-        return response(200, data=data, message="OK")
