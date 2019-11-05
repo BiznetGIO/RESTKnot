f418ddc5e212f510a798d5f36e23c33cb2dcdfa8
azzamsa <azzam@azzamsa.com>
2019-11-05 16:01:07 +0700

    Fix: feedback message

--- api/app/middlewares/auth.py
+++ api/app/middlewares/auth.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import request
 from app.helpers.rest import response
 from functools import wraps
 from netaddr import IPNetwork, IPAddress
@@ -10,7 +10,7 @@ def check_ip_range(ip, cidr):
 
 
 def check_admin_mode(ip):
-    whitelist_ip = os.environ.get("ACL", os.getenv("ACL", "127.0.0.1/24"))
+    whitelist_ip = os.environ.get("ACL")
     cidr = whitelist_ip.split(",")
     check_ip = None
     for i in cidr:
@@ -27,7 +27,7 @@ def auth_required(f):
     def decorated_function(*args, **kwargs):
         check_admin = check_admin_mode(request.remote_addr)
         if not check_admin:
-            return response(400, message="Your not access")
+            return response(400, message="Access denied")
         return f(*args, **kwargs)
 
     return decorated_function
