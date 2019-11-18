d9b0496b1fa4e73f71935bd3b838451e30647280
azzamsa <azzam@azzamsa.com>
2019-11-18 11:51:47 +0700

    fix: drop duplicate code

--- agent/dnsagent/clis/start.py
+++ agent/dnsagent/clis/start.py
@@ -18,70 +18,50 @@ class Start(Base):
         -h --help                             Print usage
     """
 
-    def execute(self):
+    def connect_kafka(self):
         broker_host = os.environ.get("RESTKNOT_KAFKA_BROKER")
         broker_port = os.environ.get("RESTKNOT_KAFKA_PORTS")
         broker = f"{broker_host}:{broker_port}"
         topic = os.environ.get("RESTKNOT_KAFKA_TOPIC")
         group = os.environ.get("RESTKNOT_KAFKA_GROUP")
-        flag = os.environ.get("RESTKNOT_KAFKA_FLAGS")
 
         if (broker_host and broker_port) is None:
             utils.log_err("Can't find kafka host and port")
             return
 
-        if self.args["slave"]:
-            try:
-                utils.log_info("Connecting to broker : " + broker)
-                consumer = kafka_lib.get_kafka_consumer(broker, topic, group)
-            except Exception as e:
-                utils.log_err("Not Connecting to broker : " + broker)
-                utils.log_err("Can't Connect to broker : " + broker)
-                utils.log_err("Error: " + str(e))
-                exit()
-            try:
-                for message in consumer:
-                    type_command = None
-                    message = message.value
-                    for i in message:
-                        try:
-                            type_command = message[i]["type"]
-                        except Exception:
-                            print("Set Your Types Command")
-                    if type_command == "general":
-                        knot_lib.parsing_data_general(message, broker)
-                    elif type_command == "cluster":
-                        knot_lib.parsing_data_cluster(message, broker, flags=flag)
-                    else:
-                        print("Type Command Not Found")
-            except KeyboardInterrupt:
-                print("Exited")
-            # except Exception as e:
-            #     env_lib.utils.log_err(str(e))
+        try:
+            utils.log_info("Connecting to broker : " + broker)
+            consumer = kafka_lib.get_kafka_consumer(broker, topic, group)
+            return consumer
+        except Exception as e:
+            utils.log_err(f"Can't Connect to broker: {e}")
             exit()
 
-        if self.args["master"]:
-            try:
-                utils.log_info("Connecting to broker : " + broker)
-                consumer = kafka_lib.get_kafka_consumer(broker, topic, group)
-            except Exception as e:
-                utils.log_err(f"Can't Connect to broker: {e}")
-                exit()
+    def parse_message(self, consumer):
+        flag = os.environ.get("RESTKNOT_KAFKA_FLAGS")
+        command_type = None
 
-            try:
-                for message in consumer:
-                    type_command = None
-                    message = message.value
-                    for i in message:
-                        try:
-                            type_command = message[i]["type"]
-                        except Exception:
-                            print("Set Your Command Type")
-                    if type_command == "general":
-                        knot_lib.parsing_data_general(message, broker)
-                    elif type_command == "cluster":
-                        knot_lib.parsing_data_cluster(message, broker, flags=flag)
-                    else:
-                        print("Command Type Not Found")
-            except Exception as e:
-                utils.log_err(str(e))
+        for message in consumer:
+            message = message.value
+            for i in message:
+                try:
+                    command_type = message[i]["type"]
+                except Exception as e:
+                    utils.log_err(f"Can't find command type: {e}")
+
+            if command_type == "general":
+                knot_lib.parsing_data_general(message)
+            elif command_type == "cluster":
+                knot_lib.parsing_data_cluster(message, flags=flag)
+            else:
+                utils.log_err(f"Unrecognized command type: {command_type}")
+
+    def execute(self):
+        consumer = self.connect_kafka()
+        self.parse_message(consumer)
+
+        if self.args["slave"]:
+            self.parse_message(consumer)
+
+        if self.args["master"]:
+            self.parse_message(consumer)
