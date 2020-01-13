156a2cd98b13954fd179cffdf6d5512b7571fe11
azzamsa <oktinastuzi@pm.me>
2020-01-14 04:48:34 +0700

    Add: log for success and failure in agent operation

--- agent/dnsagent/libs/knot.py
+++ agent/dnsagent/libs/knot.py
@@ -1,17 +1,27 @@
 from dnsagent.libs import control
+from dnsagent.libs import utils
 
 
 def execute(message):
-    control.send_block(
-        cmd=message.get("cmd"),
+    cmd = message.get("cmd")
+    zone = message.get("zone")
+    item = message.get("item")
+    data = message.get("data")
+
+    response = control.send_block(
+        cmd=cmd,
         section=message.get("section"),
-        item=message.get("item"),
+        item=item,
         identifier=message.get("identifier"),
-        zone=message.get("zone"),
+        zone=zone,
         owner=message.get("owner"),
         ttl=message.get("ttl"),
         rtype=message.get("rtype"),
-        data=message.get("data"),
+        data=data,
         flags=message.get("flags"),
         filter_=message.get("filter"),
     )
+
+    if response != "{}":
+        utils.log_err(f"Failed: {response}")
+    utils.log_info(f"Created: {cmd} {zone or ''} {item or ''} {data or ''}")
