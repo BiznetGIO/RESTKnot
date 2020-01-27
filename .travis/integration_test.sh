97c646dcec26d150899d3e99b3ca1a821164971a
azzamsa <oktinastuzi@pm.me>
2020-01-27 12:43:47 +0700

    Add: functional tests

--- .travis/integration_test.sh
+++ .travis/integration_test.sh
@@ -5,7 +5,7 @@
 # resulting test always failed in Travis environment (can't connect to
 # cockroach DB). So we use PostgreSQL with our original schema.sql ported
 psql -c 'create database knotdb;' -U postgres
-psql knotdb < api/tests/test_schema.sql
+psql knotdb < api/tests/integration/test_schema.sql
 
 pip install -r api/requirements.txt
 pip install -r api/requirements-dev.txt
