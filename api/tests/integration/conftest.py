e2cb81df16f174ea542fcb4463823f4f72c17b7b
azzamsa <oktinastuzi@pm.me>
2020-02-26 13:26:21 +0700

    Fix: regression test
    Load all env before app creation. Some part of App creation needs it.

--- api/tests/integration/conftest.py
+++ api/tests/integration/conftest.py
@@ -18,12 +18,13 @@ def clean_users():
 
 @pytest.fixture
 def client():
-    app = create_app()
-    client = app.test_client()
-
     current_path = pathlib.Path(__file__)
     dotenv_path = current_path.parents[1].joinpath("test_env.env")
     load_dotenv(dotenv_path)
+
+    app = create_app()
+    client = app.test_client()
+
     yield client
 
     # teardown
