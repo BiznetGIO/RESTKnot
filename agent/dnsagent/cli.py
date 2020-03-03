82363b82aa6e54b55a1f11946f3e9acfdf7419fa
azzamsa <oktinastuzi@pm.me>
2020-03-03 16:33:15 +0700

    Fix: don't store log to file

--- agent/dnsagent/cli.py
+++ agent/dnsagent/cli.py
@@ -25,24 +25,14 @@ from dnsagent import __version__ as VERSION
 
 
 def configure_logger():
-    log_path = os.environ.get("RESTKNOT_LOG_FILE")
-    if not log_path:
-        raise ValueError(f"RESTKNOT_LOG_FILE is not set")
-
-    file_handler = RotatingFileHandler(log_path, maxBytes=1000000, backupCount=3)
-    file_handler.setLevel(logging.WARNING)
-
-    file_format = logging.Formatter(
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_format = logging.Formatter(
         "[%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
     )
-    file_handler.setFormatter(file_format)
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_format = logging.Formatter("%(name)s - %(levelname)s - %(message)s")
     stdout_handler.setFormatter(stdout_format)
     stdout_handler.setLevel(logging.INFO)
 
     root = logging.getLogger()
-    root.addHandler(file_handler)
     root.addHandler(stdout_handler)
     root.setLevel(logging.DEBUG)
 
