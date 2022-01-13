e95ae1f658c6338dd899e8597670e89936ecfb99
Azzam S.A <azzam@biznetgio.com>
2022-01-13 08:42:00 +0700

    Fix: migrate to psycopg3
    To get built-in prepared statement.

--- api/app/models/model.py
+++ api/app/models/model.py
@@ -1,16 +1,15 @@
+import os
 from typing import Any, Dict, List, Optional, Union
 
-import psycopg2
-
-from app import database
-from app.vendors.prepare import PreparingCursor
+import psycopg
 
 
-def get_db() -> psycopg2.connect:
+def connect() -> psycopg.connect:
+    """Connect to database."""
     try:
-        connection = database.connect()
-        cursor = connection.cursor(cursor_factory=PreparingCursor)
-        return cursor, connection
+        db_uri = (os.environ.get("DB_URI"),)
+        with psycopg.connect(db_uri) as connection:
+            return conn.cursor, connection
     except Exception as exc:
         raise ValueError(f"{exc}")
 
@@ -25,26 +24,25 @@ def zip_column_name(table: str, rows: str) -> List:
 
 def get_columns(table: str) -> List:
     column = None
-    cursor, _ = get_db()
+    cursor, _ = connect()
     try:
         query = f"SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name='{table}'"
-        cursor.execute(query)
+        cursor.execute(query, prepare=True)
         column = [row[0] for row in cursor.fetchall()]
-    except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception, psycopg.DatabaseError) as error:
         raise ValueError(f"{error}")
     return column
 
 
 def get_all(table: str) -> List:
     results = []
-    cursor, connection = get_db()
+    cursor, connection = connect()
     try:
         query = f'SELECT * FROM "{table}"'
-        cursor.prepare(query)
-        cursor.execute()
+        cursor.execute(prepare=True)
         rows = cursor.fetchall()
         results = zip_column_name(table, rows)
-    except (psycopg2.DatabaseError, psycopg2.OperationalError) as error:
+    except (psycopg.DatabaseError, psycopg.OperationalError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
@@ -54,17 +52,16 @@ def get_all(table: str) -> List:
 
 def get_one(table: str, field: Optional[str] = None, value=None) -> Optional[Dict]:
     result: Dict[Any, Any] = {}
-    cursor, connection = get_db()
+    cursor, connection = connect()
     column = get_columns(table)
     try:
         query = f'SELECT * FROM "{table}" WHERE "{field}"=%(value)s'
-        cursor.prepare(query)
-        cursor.execute({"value": value})
+        cursor.execute(query, prepare=True)
         rows = cursor.fetchone()
         if not rows:
             return None
         result = dict(zip(column, list(rows)))
-    except (psycopg2.DatabaseError, psycopg2.OperationalError) as error:
+    except (psycopg.DatabaseError, psycopg.OperationalError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
@@ -73,7 +70,7 @@ def get_one(table: str, field: Optional[str] = None, value=None) -> Optional[Dic
 
 
 def insert(table: str, data: Dict) -> int:
-    cursor, connection = get_db()
+    cursor, connection = connect()
     rows: List = []
     rows_value = []
 
@@ -91,9 +88,8 @@ def insert(table: str, data: Dict) -> int:
         query = (
             f'INSERT INTO "{table}" ({_rows}) VALUES ({_str_placeholer}) RETURNING *'
         )
-        cursor.prepare(query)
-        cursor.execute((tuple(rows_value)))
-    except (Exception, psycopg2.DatabaseError) as error:
+        cursor.execute(query, prepare=True)
+    except (Exception, psycopg.DatabaseError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
@@ -103,7 +99,7 @@ def insert(table: str, data: Dict) -> int:
 
 
 def update(table: str, data: Dict) -> int:
-    cursor, connection = get_db()
+    cursor, connection = connect()
     data_ = data["data"]
     rows = []
     set_value = []
@@ -119,9 +115,8 @@ def update(table: str, data: Dict) -> int:
     try:
         set_ = ",".join(set_value)
         query = f'UPDATE "{table}" SET {set_} WHERE {field}=%(field_data)s'
-        cursor.prepare(query)
-        cursor.execute({"field_data": field_data})
-    except (Exception, psycopg2.DatabaseError) as error:
+        cursor.execute(query, prepare=True)
+    except (Exception, psycopg.DatabaseError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
@@ -133,13 +128,12 @@ def update(table: str, data: Dict) -> int:
 def delete(
     table: str, field: str = None, value: Optional[Union[str, int]] = None
 ) -> int:
-    cursor, connection = get_db()
+    cursor, connection = connect()
     rows_deleted = 0
     try:
         query = f'DELETE FROM "{table}" WHERE {field}=%(value)s'
-        cursor.prepare(query)
-        cursor.execute({"value": value})
-    except (Exception, psycopg2.DatabaseError) as error:
+        cursor.execute(query, prepare=True)
+    except (Exception, psycopg.DatabaseError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
@@ -150,11 +144,10 @@ def delete(
 
 def is_unique(table: str, field: str = None, value: str = None) -> bool:
     """Check if data only appear once."""
-    cursor, _ = get_db()
+    cursor, _ = connect()
 
     query = f'SELECT * FROM "{table}" WHERE "{field}"=%(value)s'
-    cursor.prepare(query)
-    cursor.execute({"value": value})
+    cursor.execute(query, prepare=True)
     rows = cursor.fetchall()
 
     if rows:  # initial database will return None
@@ -167,13 +160,12 @@ def is_unique(table: str, field: str = None, value: str = None) -> bool:
 def plain_get(table: str, query: str, value: Optional[dict] = None):
     """Accept plain SQL to be sent as prepared statement."""
     results = []
-    cursor, connection = get_db()
+    cursor, connection = connect()
     try:
-        cursor.prepare(query)
-        cursor.execute(value)
+        cursor.execute(query, prepare=True)
         rows = cursor.fetchall()
         results = zip_column_name(table, rows)
-    except (psycopg2.DatabaseError, psycopg2.OperationalError) as error:
+    except (psycopg.DatabaseError, psycopg.OperationalError) as error:
         connection.rollback()
         raise ValueError(f"{error}")
     else:
